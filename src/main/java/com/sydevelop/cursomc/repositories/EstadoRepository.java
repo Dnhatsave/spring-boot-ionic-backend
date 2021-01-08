3b13c6f8bae6c6769a6dc9753e31bcae2d81fd9f
dercioink <dercionhatsave@gmail.com>
2021-01-08 11:51:45 +0200

    Criacao das entidades estado e cidade

---
+++ src/main/java/com/sydevelop/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.sydevelop.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sydevelop.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+
+}
