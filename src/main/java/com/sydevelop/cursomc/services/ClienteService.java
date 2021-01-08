57d3932db616aace099037d882df68237c34d921
dercioink <dercionhatsave@gmail.com>
2021-01-08 13:37:21 +0200

    Endpoit cliente/id criado

---
+++ src/main/java/com/sydevelop/cursomc/services/ClienteService.java
@@ -0,0 +1,26 @@
+package com.sydevelop.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.sydevelop.cursomc.domain.Cliente;
+import com.sydevelop.cursomc.repositories.ClienteRepository;
+import com.sydevelop.cursomc.services.exceptions.ObjectNotFoundException;
+
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		
+		return obj.orElseThrow( ()-> new ObjectNotFoundException("Objecto nao encontrado! Id" + id
+				+ "Tipo:" + Cliente.class.getName()));
+	}
+
+}
