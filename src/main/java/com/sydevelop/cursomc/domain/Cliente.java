fcfce9d70024fd2f58dfd2fb00d8ac0777fda891
dercioink <dercionhatsave@gmail.com>
2021-01-11 16:57:39 +0200

    Atualziacao para JsonIgnore

--- src/main/java/com/sydevelop/cursomc/domain/Cliente.java
+++ src/main/java/com/sydevelop/cursomc/domain/Cliente.java
@@ -12,11 +12,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.sydevelop.cursomc.domain.enumns.TipoCliente;
 
 @Entity
@@ -31,7 +29,6 @@ public class Cliente implements Serializable {
 	private String cpfOuCpnj;
 	private Integer tipo;
 	
-	@JsonManagedReference
 	@OneToMany(mappedBy="cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 
@@ -39,7 +36,7 @@ public class Cliente implements Serializable {
 	@CollectionTable(name="CONTACTO")
 	private Set<String> contactos = new HashSet<>();
 	
-	@JsonBackReference
+	@JsonIgnore
 	@OneToMany(mappedBy="cliente")
 	private List<Pedido> pedidos = new ArrayList<>();
 	
