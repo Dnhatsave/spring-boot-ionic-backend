824f54f75077c7e7806d7735626b99e8f90dd402
dercioink <dercionhatsave@gmail.com>
2021-01-12 11:11:30 +0200

    CRUD DELETE CATEGOTIAS e Tratamento de erros

--- src/main/java/com/sydevelop/cursomc/services/CategoriaService.java
+++ src/main/java/com/sydevelop/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.sydevelop.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.sydevelop.cursomc.domain.Categoria;
 import com.sydevelop.cursomc.repositories.CategoriaRepository;
+import com.sydevelop.cursomc.services.exceptions.DataIntegrityException;
 import com.sydevelop.cursomc.services.exceptions.ObjectNotFoundException;
 
 
@@ -31,6 +33,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete (Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Nao é Possivel Excluir uma categoria que possui produtos!");
+		}
 	}
 		
 
