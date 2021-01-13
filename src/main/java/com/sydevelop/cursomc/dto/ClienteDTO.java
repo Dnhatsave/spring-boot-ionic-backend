110c95298160f683f04cbc405243c11fcd3bd575
dercioink <dercionhatsave@gmail.com>
2021-01-13 14:58:08 +0200

    PUT, DELETE e GET para cliente

---
+++ src/main/java/com/sydevelop/cursomc/dto/ClienteDTO.java
@@ -0,0 +1,57 @@
+package com.sydevelop.cursomc.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.sydevelop.cursomc.domain.Cliente;
+
+public class ClienteDTO implements Serializable {
+	static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	@NotEmpty(message = "Campo de carracter obrigatorio")
+	@Length(min =5, max = 150, message="O tamanho deve ser entre 5 e 150 charracters")
+	private String nome;
+	@NotEmpty(message = "Campo de carracter obrigatorio")
+	@Email(message = "Email invalido")
+	private String email;
+	
+	public ClienteDTO() {}
+	
+	public ClienteDTO(Cliente obj) {
+		id = obj.getId();
+		nome = obj.getNome();
+		email = obj.getEmail();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+
+}
--- src/main/java/com/sydevelop/cursomc/services/ClienteService.java
+++ src/main/java/com/sydevelop/cursomc/services/ClienteService.java
@@ -1,12 +1,21 @@
 package com.sydevelop.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.sydevelop.cursomc.domain.Categoria;
 import com.sydevelop.cursomc.domain.Cliente;
+import com.sydevelop.cursomc.domain.Cliente;
+import com.sydevelop.cursomc.dto.ClienteDTO;
 import com.sydevelop.cursomc.repositories.ClienteRepository;
+import com.sydevelop.cursomc.services.exceptions.DataIntegrityException;
 import com.sydevelop.cursomc.services.exceptions.ObjectNotFoundException;
 
 
@@ -21,6 +30,46 @@ public class ClienteService {
 		
 		return obj.orElseThrow( ()-> new ObjectNotFoundException("Objecto nao encontrado! Id" + id
 				+ "Tipo:" + Cliente.class.getName()));
+	}
+	
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+	
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	public void delete (Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Nao é Possivel Excluir este cliente!");
+		}
+	}
+	
+	public List<Cliente> findAll(){
+		return repo.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest =  PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+		
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(),null,null);
+	}
+
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
 	}
 
 }
