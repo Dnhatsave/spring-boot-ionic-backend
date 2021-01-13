49930c7ed8a6272728d895ad3a4fb3a42a14cad5
dercioink <dercionhatsave@gmail.com>
2021-01-13 17:25:45 +0200

    Validacao da Redundancia do Email

--- src/main/java/com/sydevelop/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/sydevelop/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.sydevelop.cursomc.domain.Cliente;
 import com.sydevelop.cursomc.domain.enumns.TipoCliente;
 import com.sydevelop.cursomc.dto.ClienteNewDTO;
+import com.sydevelop.cursomc.repositories.ClienteRepository;
 import com.sydevelop.cursomc.resources.exceptions.FieldMessage;
 import com.sydevelop.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -21,11 +29,17 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		List<FieldMessage> list = new ArrayList<>();
 
 		if(objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
-			list.add(new FieldMessage("cpfOuCpnj","CPF Invalido"));
+			list.add(new FieldMessage("cpfOuCnpj","CPF Invalido"));
 		}
 		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
-			list.add(new FieldMessage("cpfOuCpnj","CPNJ Invalido"));
+			list.add(new FieldMessage("cpfOuCnpj","CPNJ Invalido"));
 		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+			if(aux != null) {
+				list.add(new FieldMessage("email","Email ja Registado"));
+			}
+
 
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
